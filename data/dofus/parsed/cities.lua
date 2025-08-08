4aa5b831e5571b446e165f8632afcd6272252094
Nraw <130287219+Nelraw@users.noreply.github.com>
2025-08-08 19:56:53 +0200

    init

---
+++ data/dofus/parsed/cities.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        area = "Bonta",
+        maps = {
+            zaap = 212600323,
+            bank = {
+                indoor = 217059328,
+                outoor = 212600322,
+            },
+            bidhouses = {
+                cosmetic = 212731651,
+                resources = 212601350,
+                equipments = 212600837,
+                consumables = 212600839,
+                souls = 220200961,
+                pets = 212601345,
+                runes = 212601859,
+            }
+        }
+    },
+    {
+        area = "Brâkmar",
+        maps = {
+            zaap = 212861955,
+            bank = {
+                indoor = 212861954,
+                outoor = 214696968,
+            },
+            bidhouses = {
+                cosmetic = 214697994,
+                resources = 212861952,
+                equipments = 212860930,
+                consumables = 212863491,
+                souls = 219677441,
+                pets = 212862469,
+                runes = 212861957,
+            }
+        }
+    }
+}
